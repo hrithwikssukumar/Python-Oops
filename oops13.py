edfe91d58af9b0d11dcabc5774d4081f57fafa1b
Hrithwik <hrithwikssukumar@gmail.com>
2025-01-10 11:00:13 +0530

    added oops13.py

---
+++ oops13.py
@@ -0,0 +1,13 @@
+class Family:
+    def __init__(self,name,age,profession):
+        self.name =name
+        self.age  =age
+        self.profession=profession
+m1 =Family("Hrithwik",28,"Engineer")
+m2 =Family("Hrishik",24,"Businessman")
+m3 =Family("Sheena",48,"Teacher")
+m4 =Family("Sukumaran",62,"Farmer")
+
+print(m1.age)
+print(m3.name)
+print(m2.profession)
